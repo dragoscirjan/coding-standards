c3fa355816cfd8878aeb68e1c0373688f1bc0708
Dragos Cirjan <Dragos.Cirjan@pj-interactive.ro>
2016-03-24 18:08:20 +0200

    +

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Coding Standars';
     config.map([
-      { route: ['', 'overview'],   name: 'overview',         moduleId: 'overview',         nav: true, title: 'Overview' },
-      { route: 'file-formating',   name: 'file-formating',   moduleId: 'file-formating',   nav: true, title: 'File Formating' },
-      { route: 'coding-style',     name: 'coding-style',     moduleId: 'coding-style',     nav: true, title: 'Coding Style' }
+      { route: ['', 'overview'], name: 'overview',    moduleId: 'overview',    nav: true, title: 'Overview' },
+      { route: 'file',           name: 'file',        moduleId: 'file',        nav: true, title: 'File Formating' },
+      { route: 'naming',         name: 'naming',      moduleId: 'naming',      nav: true, title: 'Naming Convetions' },
+      { route: 'style',          name: 'style',       moduleId: 'style',       nav: true, title: 'Coding Style' },
+      { route: 'style-html',     name: 'style-html',  moduleId: 'style-html',  nav: true, title: 'Coding Style (HTML)' },
+      { route: 'recommend',      name: 'recommend',   moduleId: 'recommend',   nav: true, title: 'Recommended Tools' }
     ]);
 
     this.router = router;
