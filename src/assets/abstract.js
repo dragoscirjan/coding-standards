641a8abe6f5bdacc680414db5d94fe7bb06bcba7
Dragos Cirjan <Dragos.Cirjan@pj-interactive.ro>
2016-04-08 17:38:34 +0300

    +

--- src/assets/abstract.js
+++ src/assets/abstract.js
@@ -8,29 +8,21 @@ export class Component {
   attached() {
     let liItemCount = 1;
 
+    // Within .lang-ex elements
+    // create tabs from h5 elements, and the content after it
     $('.lang-ex').each(function() {
       let $this = $(this);
       let $ul = $('<ul class="nav nav-tabs" role="tablist"></ul>');
       let $div = $('<div class="tab-content"></div>');
       // let liFirstItem = liItemCount;
-
       $this.find('h5').each(function() {
         let $li = $(`<li role="presentation"><a href="#li-item-${liItemCount}" aria-controls="li-item-${liItemCount}" role="tab" data-toggle="tab"></a></li>`);
-        let $dv = $(`<div role="tabpanel" class="tab-pane" id="li-item-${liItemCount++}"></div>`);
+        let $dv = $(`<div role="tabpanel" class="tab-pane example-pane" id="li-item-${liItemCount++}"></div>`);
         let $d = null;
         let count = 0;
 
-        if (!this.innerHTML.match(/TODO/)) {
-          $li.find('a').html(this.innerHTML);
-        } else {
-          $li.find('a')
-          .addClass('disabled')
-          .attr('data-toggle', '')
-          .tooltip({
-            title: 'Content not available. Care to help designing it?'
-          })
-          .html(this.innerHTML.replace(/\s*TODO\s*/, ''));
-        }
+        $li.find('a').html(this.innerHTML.replace(/\s*TODO\s*/, ''));
+
         while (count < 50) {
           $d = ($d && $d.length) ? $d.next() : $(this).next();
           if (!$d || $d.prop('tagName') === 'H5') {
@@ -43,16 +35,49 @@ export class Component {
         $div.append($dv);
       });
 
-      $this.html('');
-      $this.append($ul);
-      $this.append($div);
+      $this.html('').append($ul).append($div);
+      $ul.find('li:first').addClass('active');
+      $div.find('div:first').addClass('active');
+    });
 
+    // Within .lang-ex elements
+    // create tabs from h5 elements, and the content after it
+    $('.lang-ex .example-pane').each(function() {
+      let $this = $(this);
+      let $ul = $('<ul class="nav nav-tabs" role="tablist"></ul>');
+      let $div = $('<div class="tab-content"></div>');
+
+      if ($this.find('h6').length == 0) {
+        return;
+      }
+
+      // let liFirstItem = liItemCount;
+      $this.find('h6').each(function() {
+        let $li = $(`<li role="presentation"><a href="#li-item-${liItemCount}" aria-controls="li-item-${liItemCount}" role="tab" data-toggle="tab"></a></li>`);
+        let $dv = $(`<div role="tabpanel" class="tab-pane" id="li-item-${liItemCount++}"><div class="code-pane"></div></div>`);
+        let $d = null;
+        let count = 0;
+
+        $li.find('a').html(this.innerHTML.replace(/\s*TODO\s*/, ''));
+
+        while (count < 50) {
+          $d = ($d && $d.length) ? $d.next() : $(this).next();
+          if (!$d || $d.prop('tagName') === 'H6') {
+            break;
+          }
+          $dv.find('.code-pane').append($d);
+          count ++;
+        }
+        $ul.append($li);
+        $div.append($dv);
+      });
+
+      $this.html('').append($ul).append($div);
       $ul.find('li:first').addClass('active');
       $div.find('div:first').addClass('active');
     });
 
     $('pre > code').each(function(i, block) {
-      console.log(block);
       let $code = $(block);
       if ($code.hasClass('lang-cfml')) {
         $code.addClass('lang-xml');
